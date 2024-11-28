36d01d181904bbbede4b4c8efb963cef5eb017c2
Ragini340 <ragini31bxr@gmail.com>
2024-11-28 19:46:05 +0530

    Add project files.

---
+++ Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using SampleMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVCApplication.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Random()
+        {
+            var game = new Game()
+            {
+                Name = "Kabaddi"
+            };
+            return View(game);
+        }
+    }
+}
